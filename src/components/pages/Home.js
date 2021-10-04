83f3d4788bd126513e3f5fa5fefbc932d5d15c10
Dimas Neto <dimas_fcn@outlook.com>
2021-10-03 22:20:40 -0300

    inicio

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import styles from "./Home.module.css";
+
+function Home() {
+  return (
+    <section>
+      <h1>
+        Bem-vindo ao <span>Costs</span>
+      </h1>
+      <p>Começe a gerenciar os seus projetos agora mesmo!</p>
+    </section>
+  );
+}
+
+export default Home;
